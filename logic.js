615dd723213d36aa920ee870e6db347d50611633
Justin Ling <48619286+Justin-zi@users.noreply.github.com>
2022-02-14 15:07:36 +1100

    Update style and facts generator.

--- logic.js
+++ logic.js
@@ -30,8 +30,6 @@ document.querySelector(".container").addEventListener('click', function(e) {
         animatePress(userChosenColour);
         checkAnswer(userClickedPattern.length-1);
     }
-
-    
 });
 
 
@@ -47,6 +45,7 @@ const nextSequence = () => {
     $("#" + randomChosenColor).fadeOut(150).fadeIn(150);
 
     playSound(randomChosenColor);
+    facts();
 
     // document.querySelector("h1").textContent = `Level ${levelNumber}`;
 }
@@ -94,4 +93,24 @@ let startOver = () => {
     levelNumber = 0;
     gamePattern = [];
     gameStart = false;
+}
+
+function facts() {
+    const funFacts = {
+        '1': "Fun fact - Short-term memory only lasts 20 to 30 seconds.",
+        '2': "Fun fact - The human brain’s storage capacity is virtually limitless.",
+        '3': "Fun fact - Coffee only helps to increase alertness, it doesn’t maintain memory performances.",
+        '4': "Fun fact - Sleep is significant to memory. Sleep helps in the retrieval and storage of long-term memories.",
+        '5': "Fun fact - Many people link aging with memory loss. However, the memory loss people experience as they age is because they tend to exercise their brains less.",
+        '6': "Fun fact - Your memory has the ability to associate a scent with a particular occurrence or event.",
+        '7': "Fun fact - Just like any other part of your body, exercise is also important for your brain. The more you try to think about a memory, you can remember it more accurately. In fact, when you think, it creates a stronger connection between active neurons."
+    };
+
+    const props = Object.values(funFacts);
+    const factProperties = props[Math.floor(Math.random() * props.length)];
+    console.log(factProperties);
+
+    if(levelNumber >= 2) {
+        document.querySelector(".smaller").textContent = factProperties;
+    }
 }
